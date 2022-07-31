49ace3515fe7aa306691a01d9c3089f264e34d52
Igor <i.pavlyshyn05@gmail.com>
2022-07-31 20:49:15 +0300

    first commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
-const setActive = ({isActive})=> isActive ? 'active-link' : '';
+const setActive = ({ isActive }) => (isActive ? "active-link" : "");
 
 const Layout = () => {
   return (
     <>
       <div className="Header">
-        <NavLink to="/Homework-19" className={setActive} >Home</NavLink>
-        <NavLink to="/Homework-19/posts" className={setActive} >Posts</NavLink>
-        <NavLink to="/Homework-19/photos" className={setActive} >Photos</NavLink>
-        <NavLink to="/Homework-19/contacts" className={setActive} >Contacts</NavLink>
+        <NavLink to="/Homework-19" className={setActive}>
+          Home
+        </NavLink>
+        <NavLink to="/Homework-19/posts" className={setActive}>
+          Posts
+        </NavLink>
+        <NavLink to="/Homework-19/photos" className={setActive}>
+          Photos
+        </NavLink>
+        <NavLink to="/Homework-19/contacts" className={setActive}>
+          Contacts
+        </NavLink>
       </div>
-      <div className='container'>
+      <div className="container">
         <Outlet />
       </div>
-      
     </>
   );
 };
